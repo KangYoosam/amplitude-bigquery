ce81ba2fe19a347d10924ac944f7e68904af936c
kang yoosam <yooska14@gmail.com>
2019-12-25 11:34:49 +0900

    fix to daily schedule

--- amplitude-bigquery.py
+++ amplitude-bigquery.py
@@ -221,7 +221,8 @@ storage_client = storage.Client()
 dataset_ref = bigquery_client.dataset('amplitude')
 
 # n日前からm日前までの期間データをAmplitude => Cloud Storage => BigQueryに移行する
-# for i in range(9,24):
+# for i in range(25, 54):
+    # date = (datetime.utcnow().date() - timedelta(days=i)).strftime("%Y%m%d")
 date = (datetime.utcnow().date() - timedelta(days=2)).strftime("%Y%m%d")
 print('starts importing day of : ' + date)
 main(date)
